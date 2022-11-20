1e64f547f9360cd25a67b66263d6d73456342e13
greyich <greyich.potapov@yandex.com>
2022-11-20 14:43:43 +0300

    исправлен код функции greetings, для wemakepythonstyleguide assert поменял на raise

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,33 @@
+"""тесты для генератора приветствий."""
 import pytest
-from main import *
+from main import greeting
 
 
-@pytest.mark.parametrize( 'name,expected', [('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга')],)
+@pytest.mark.parametrize(
+    'name,expected',
+    [
+     ('Никита', 'Привет, Никита'), ('Ольга', 'Привет, Ольга'),
+    ])
 def test_greeting(name: str, expected: str):
-    """Текст приветствия зависит от имени."""
-    assert Greeting(name) == expected
+    """Текст приветствия зависит от имени.
+
+    Args:
+        name (str): Имя пользователя
+        expected (str): Текст приветствия
+
+    Raises:
+        RuntimeError: if greeting(name) != expected
+    """
+    if greeting(name) != expected:
+        raise RuntimeError
+
 
 def test_capitalize():
-    """Все слова в имени начинаются с большой буквы."""
+    """Все слова в имени начинаются с большой буквы.
+
+    Raises:
+        RuntimeError: if greeting(name) != 'Привет, Яндекс Практикум'
+    """
     name = 'яндекс практикум'
-    assert Greeting(name) == 'Привет, Яндекс Практикум'
+    if greeting(name) != 'Привет, Яндекс Практикум':
+        raise RuntimeError
